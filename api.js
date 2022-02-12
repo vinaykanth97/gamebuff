c60877aff81d8851d3c34019715ca044e51e474a
vinaypump <vinaykennedy@gmail.com>
2022-02-13 00:25:43 +0530

    Api setup

---
+++ api.js
@@ -0,0 +1,29 @@
+// Base URL
+const baseUrl = "https://api.rawg.io/api/games";
+
+// Get Month
+const GetCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+  return month < 10 ? `0${month}` : month;
+};
+
+// Get Date
+const GetCurrentDay = () => {
+  const getCurrDay = new Date().getDate();
+  return getCurrDay < 10 ? `0${getCurrDay}` : getCurrDay;
+};
+
+// Get Year
+const GetCurrentYear = new Date().getFullYear();
+const currentMonth = GetCurrentMonth();
+const currentDay = GetCurrentDay();
+
+// Date Formats
+const currentIdenticalDates = `${GetCurrentYear}-${currentMonth}-${currentDay}`;
+const previousIdenticalDates = `${
+  GetCurrentYear - 1
+}-${currentMonth}-${currentDay}`;
+
+const popularGames = `${baseUrl}?dates=${previousIdenticalDates},${currentIdenticalDates}&ordering=-rating&page_size=10`;
+
+export const popularGamesUrl = () => popularGames;
