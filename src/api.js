a4f4d253d62960b49e2a42020e4d8245584a6520
vinaypump <vinaykennedy@gmail.com>
2022-02-13 00:53:03 +0530

    Api fix

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 // Base URL
-const baseUrl = "https://api.rawg.io/api/games";
+let apiKey = "194f506dc2904b0ea10cd942deb63b14";
+const baseUrl = `https://api.rawg.io/api/games?key=${apiKey}`;
 
 // Get Month
 const GetCurrentMonth = () => {
@@ -24,6 +25,6 @@ const previousIdenticalDates = `${
   GetCurrentYear - 1
 }-${currentMonth}-${currentDay}`;
 
-const popularGames = `${baseUrl}?dates=${previousIdenticalDates},${currentIdenticalDates}&ordering=-rating&page_size=10`;
+const popularGames = `${baseUrl}&dates=${previousIdenticalDates},${currentIdenticalDates}&ordering=-rating&page_size=10`;
 
 export const popularGamesUrl = () => popularGames;
